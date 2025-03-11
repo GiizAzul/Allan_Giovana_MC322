72832fc70efb05e585fd09827815ac86b51b1836
AllanRPereira <allanflyk2@gmail.com>
2025-03-11 14:59:38 -0300

    Classe Robo

---
+++ src/Robo.java
@@ -0,0 +1,24 @@
+public class Robo {
+
+    private String nome;
+    private int posicaoX;
+    private int posicaoY;
+
+    public Robo(String nome, int posicaoX, int posicaoY) {
+        this.nome = nome;
+        this.posicaoX = posicaoX;
+        this.posicaoY = posicaoY;
+    }
+
+    public void mover(int deltaX, int deltaY) {
+        this.posicaoX += deltaX;
+        this.posicaoY += deltaY;
+    }
+
+    public void exibirPosicao() {
+        String text = "Nome: %s X:%d Y:%d";
+        String formated = String.format(text, this.nome, this.posicaoX, this.posicaoY);
+        System.out.println(formated);
+    }
+
+}
