c727efa4cea07241c07287032b22bbd78f400783
AllanRPereira <allanflyk2@gmail.com>
2025-03-30 14:25:58 -0300

    Ajuste método de atirar da classe TanqueGuerra

--- Lab02/TanqueGuerra.java
+++ Lab02/TanqueGuerra.java
@@ -13,23 +13,31 @@ public class TanqueGuerra extends RoboTerrestre {
     }
 
     public String atirar(int alvoX, int alvoY, int nTiros, Ambiente ambiente) {
-        Robo alvo = (Robo)identificarObstaculoPosicao(ambiente, alvoY, alvoX);
+        Robo alvo = (Robo) ambiente.identificarObjetoPosicao(alvoX, alvoY);
         if (municaoAtual < nTiros) {
             return "Munição insuficiente";
+
         } else if (alvoX == getPosicaoX() && alvoY == getPosicaoY()){
             return "Não é possível atirar na própria posição";
-        } else if ( alvo != null){
+
+        } else {
             int dX = getPosicaoX() - alvoX;
             int dY = getPosicaoY() - alvoY;
             if (Math.pow(dX, 2) + Math.pow(dY, 2) <= Math.pow(alcance,2)) {
                 municaoAtual -= nTiros;
-                return "Disparo do " + getNome() + " realizado no robo " + alvo.getNome() + "\n" + alvo.defender(nTiros);
+                if (alvo == null) {
+                    return String.format("Disparado realizado no alvo (%d, %d)\nNenhum alvo foi atingido!\n", alvoX, alvoY);
+                } else {
+                    String result = String.format(
+                        "Disparo realizado no alvo (%d, %d)\n" +
+                        "Robô foi %s foi atingido!\n"
+                    , alvoX, alvoY, alvo.getNome());
+                    return result;
+                }
             } else {
                 return "Alvo fora do alcance";
             }
-        } else{
-            return "Não há alvos nesta posição";
-        }   
+        }
     }
 
     public String recarregar(int nBalas){
