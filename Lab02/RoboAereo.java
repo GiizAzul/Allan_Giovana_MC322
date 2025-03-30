1a8b27ffdbe063b19f16ccd222ad3fbbe8d71116
AllanRPereira <allanflyk2@gmail.com>
2025-03-30 19:44:54 -0300

    Correção construtor RoboAereo

--- Lab02/RoboAereo.java
+++ Lab02/RoboAereo.java
@@ -6,18 +6,6 @@ public class RoboAereo extends Robo {
     private int altitude = -1;
     private int altitudeMaxima = -1;
 
-    public RoboAereo(String n, String d, int x, int y, int h) {
-        /*
-         * n -> Nome do Robô
-         * d -> Direção
-         * x -> Posição X
-         * y -> Posição Y
-         * h -> Altura Inicial
-         */
-        super(n, d, x, y);
-        this.altitude = h;
-    }
-
     public RoboAereo(String n, String d, int x, int y, int h, int hmax) {
         /*
          * n -> Nome do Robô
@@ -33,19 +21,7 @@ public class RoboAereo extends Robo {
         this.altitudeMaxima = hmax;
     }
 
-    private boolean validacao() {
-        if (this.altitudeMaxima == -1) {
-            return false;
-        }
-        return true;
-    }
-
     public void subir(int metros) {
-        if (!validacao()) { 
-            System.out.println("Atributos obrigatórios não foram setados");
-            return;
-        }
-
         this.altitude += metros;
         if (this.altitude>this.altitudeMaxima){
             System.out.println("Altura máxima foi atingida!");
@@ -54,11 +30,6 @@ public class RoboAereo extends Robo {
     }
 
     public void descer(int metros) {
-        if (!validacao()) { 
-            System.out.println("Atributos obrigatórios não foram setados");
-            return;
-        }
-
         this.altitude -= metros;
         if (this.altitude < 0){
             this.altitude = 0;
