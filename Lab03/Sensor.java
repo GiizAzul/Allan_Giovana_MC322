3485b1b7af0002b3794fb08e6ac28c3b5b135d00
AllanRPereira <allanflyk2@gmail.com>
2025-04-23 22:59:23 -0300

    Classe base do sensor #3

--- Lab03/Sensor.java
+++ Lab03/Sensor.java
@@ -1,13 +1,48 @@
+/**
+ * Classe abstrata genérica que representa um sensor básico para robôs.
+ * Cada tipo de sensor específico pode retornar diferentes tipos de dados.
+ * 
+ * @param <T> O tipo de dado retornado pelo sensor ao ser acionado
+ */
 abstract class Sensor<T> {
-    private int raio;
+    /** Indica se o sensor está ativo ou não */
+    private boolean ativo;
 
-    Sensor(int raio) { // constructor que inicializa o raio do sensor
-        this.raio = raio;
+    /**
+     * Construtor padrão. Inicializa o sensor como ativo.
+     */
+    Sensor() { 
+        this.ativo = true;
     }
 
-    int getRaio() { // método que retorna o raio do sensor
-        return raio;
+    /**
+     * Ativa o sensor para que possa ser acionado e retornar dados.
+     */
+    void ativar() { 
+        this.ativo = true;
     }
 
-    abstract T monitorar();
+    /**
+     * Desativa o sensor, impedindo acionamento.
+     */
+    void desativar() { 
+        this.ativo = false;
+    }
+
+    /**
+     * Verifica se o sensor está ativo ou não.
+     * 
+     * @return true se o sensor estiver ativo, false caso contrário
+     */
+    boolean isAtivo() { 
+        return this.ativo;
+    }
+
+    /**
+     * Realiza o acionamento do sensor e retorna dados do tipo especificado.
+     * Cada subclasse deve implementar este método de acordo com seu tipo.
+     * 
+     * @return Dados coletados pelo sensor
+     */
+    abstract T acionar();
 }
