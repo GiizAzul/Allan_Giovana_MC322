e2a3255d4203411f321f302144906ecdbe43a082
AllanRPereira <allanflyk2@gmail.com>
2025-05-30 11:38:59 -0300

    Refatoração Interfaces

--- Lab04/interfaces/Atacante.java
+++ Lab04/interfaces/Atacante.java
@@ -1,8 +1,11 @@
 package interfaces;
 
 import ambiente.Ambiente;
+import excecoes.*;
+import excecoes.sensor.SensorInativoException;
 
 public interface Atacante {
-    String atirar(int alvoX, int alvoY, int alvoZ, int nTiros, Ambiente ambiente);
+    String atirar(int alvoX, int alvoY, int alvoZ, int nTiros, Ambiente ambiente)
+    throws SensorInativoException, MunicaoInsuficienteException, AlvoInvalidoException;
     String recarregar(int nBalas);
 }
